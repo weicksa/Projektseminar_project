fc2d518bd177bc6098330235d360328e5bea2b96
Sebastian Pado <pado@adler.ims.uni-stuttgart.de>
2022-11-03 16:43:43 +0100

    first commit with project skeleton

---
+++ project/tagger/Tagger.py
@@ -0,0 +1,26 @@
+'''
+Created on Oct 29, 2020
+
+@author: nastasvi
+'''
+
+import io
+
+from .data import Sentence
+from .data import Token
+
+class Tagger(object):
+    '''
+    classdocs
+    '''
+
+      
+    @staticmethod
+    def extractInstances(data, goldLabel, predLabel):
+        pass
+
+    @staticmethod
+    def readCoNLL(filename):
+        pass
+
+
